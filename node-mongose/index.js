91ca061aec4d9d3deffc1b5bc26e018df6d24338
unknown <likhith2901@gmail.com>
2020-08-29 15:27:50 +0530

    second commit

--- node-mongose/index.js
+++ node-mongose/index.js
@@ -6,21 +6,38 @@ const url='mongodb://localhost:27017/myData';
 const connect=mongoose.connect(url);
 
 connect.then((db)=>{
-    var newDish=Dishes({
-        name:"Samosa",
-        description:"Very Hot and Spicy"
-    });
-
-    newDish.save()    //save data and will return a call back and it is used in next line. In that call back dish is returned
-    .then((dish)=>{
+    Dishes.create({
+        name:"MagoBite",
+        description:"Orange Candy"
+    }) //create will save and return a dish so they can be accessed using the .then
+    .then((dish) => {
         console.log(dish);
-        return Dishes.find({}).exec()  //this will return a call back and that call back is used on then in next line
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated Candy'}
+        },{ 
+            new: true 
+        })
+        .exec();  
+        //when we find the id and update it 
+        //it will return the dish and for that dish we are updating the comments
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a Yummy feeling!',
+            author: 'Nestle'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
         mongoose.connection.close();
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err);
-    })
+    });
 });
--- node-mongose/models/dishes.js
+++ node-mongose/models/dishes.js
@@ -1,6 +1,21 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
-
+const commentSchema=new Schema({
+    rating:{
+        type:Number,
+        required:true
+    },
+    comment:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+});
 const dishSchema=new Schema({
     name:{
         type:String,
@@ -11,10 +26,11 @@ const dishSchema=new Schema({
     description:{
         type:String,
         required:true
-    }
+    },
+    comments: [ commentSchema ]
 },{
     timestamps:true
-});
+}); 
 
 var Dishes=mongoose.model('Dish',dishSchema);
 
