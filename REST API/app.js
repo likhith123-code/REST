c957d011f41814a0520b5b97c1f8828aeafcb306
unknown <likhith2901@gmail.com>
2020-09-24 10:59:30 +0530

    commit

--- REST API/app.js
+++ REST API/app.js
@@ -8,9 +8,7 @@ const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 mongoose.connect(
-  "mongodb://node-shop:" +
-    process.env.MONGO_ATLAS_PW +
-    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
+  "mongodb+srv://Likhith1520:Likhith1520@rest-api.miiel.mongodb.net/Products?retryWrites=true&w=majority",
   {
     useMongoClient: true
   }
